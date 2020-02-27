eda4123d4b82fd94a09f9a0a415bc366d0f4a37e
dannyunico <danielbermudezreyes@gmail.com>
2020-02-27 11:52:20 -0400

    slider-horizontal

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,17 @@
-$('.slider-banner').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: true,
-  
 
-  responsive: [
-    {
-      breakpoint: 1025,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-  ]
-});
+
+const slider = $(".slider-banner");
+slider
+  .slick({
+    dots: true
+  });
+
+slider.on('wheel', (function(e) {
+  e.preventDefault();
+
+  if (e.originalEvent.deltaY < 0) {
+    $(this).slick('slickNext');
+  } else {
+    $(this).slick('slickPrev');
+  }
+}));
