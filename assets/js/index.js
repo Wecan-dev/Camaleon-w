0f2e6ce81b753ea83254c36181000b1257d6f571
dannyunico <danielbermudezreyes@gmail.com>
2020-02-28 10:46:37 -0400

    css

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,8 +11,34 @@ slider.on('wheel', (function(e) {
   e.preventDefault();
 
   if (e.originalEvent.deltaY < 0) {
-    $(this).slick('slickNext');
+      $(this).slick('slickPrev');
   } else {
-    $(this).slick('slickPrev');
+ 
+     $(this).slick('slickNext');
+  }
+}));
+
+
+
+const sliderPortafolio = $(".slider-portafolio");
+sliderPortafolio
+  .slick({
+    arrows:false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode:false,
+    variableWidth:true,
+    dots:true,
+  });
+
+sliderPortafolio.on('wheel', (function(e) {
+  e.preventDefault();
+
+  if (e.originalEvent.deltaY < 0) {
+      
+       $(this).slick('slickPrev');
+  } else {
+        $(this).slick('slickNext');
   }
 }));
